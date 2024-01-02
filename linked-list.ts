f1d45f583c54c1a225ed306e7680d216c07e4f2e
DJ Hemath <djhemath@gmail.com>
2024-01-02 08:48:06 +0530

    feat: ref for tail and prop for length and helpers

--- linked-list.ts
+++ linked-list.ts
@@ -1,13 +1,22 @@
 import Node from './common/singly-linked-node';
 
 export class LinkedList {
-  public head: Node;
+  public head: Node | null = null;
+  public tail: Node | null = null;
+  public length: number = 0;
 
   constructor(initialValue: any) {
     this.head = new Node(initialValue);
+    this.tail = this.head;
+
+    this.incrementLength();
   }
 
-  print() {
-    console.log(JSON.stringify(this, null, 2));
+  private incrementLength() {
+    this.length += 1;
+  }
+
+  private decrementLength() {
+    this.length -= 1;
   }
 }
